e876fbec15af18d1a83a3f8ae5c784a8efb882d3
Andrei Luca <thendrluca@gmail.com>
2015-03-30 11:04:48 -0700

    from ground to stars )

---
+++ TileGame/TileGame/LobbyViewController.swift
@@ -0,0 +1,50 @@
+//
+//  LobbyViewController.swift
+//  TileGame
+//
+//  Created by Andrei Luca on 3/30/15.
+//  Copyright (c) 2015 Tile Game. All rights reserved.
+//
+
+import UIKit
+
+class LobbyViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        var cell = (collectionView.dequeueReusableCellWithReuseIdentifier("LobbyCell", forIndexPath: indexPath) as LobbyCollectionViewCell)
+        cell.levelNumber.text = String(indexPath.row)
+        
+        return cell
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TileGame/TileGame/MenuViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MenuViewController.swift
+//  TileGame
+//
+//  Created by Andrei Luca on 3/30/15.
+//  Copyright (c) 2015 Tile Game. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
